d050038d931bec1919d5e860feef1adf32b9bee6
ejirocastro <awenayeriejiro@gmail.com>
2023-07-14 21:25:17 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success) valiod
+ */
+int main(void)
+{
+	char n;
+
+	char m;
+
+	n = 'a';
+	m = 'A';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}

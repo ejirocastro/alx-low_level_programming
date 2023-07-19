cfd59d27c429b28eb6a3359a1a9bae5b6900e904
ejirocastro <awenayeriejiro@gmail.com>
2023-07-19 14:57:56 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+/**
+ * _islower - checks lowercases
+ * @c: The character to be checked
+ * Return: 1 for lowercase or 0 for anything else
+ */
+int _islower(int c)
+{
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

dec5a4e5231c3ab5e656a977ac7060e3ecefa829
ejirocastro <awenayeriejiro@gmail.com>
2023-07-20 16:10:34 -0700

    0-isup

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include <ctype.h>
 
 /**
- * _isupper - This is a function
- * @c: is the character checked
+ * _isupper - checks for uppercase character
+ * @c: character checked
  * Return: 1 if c is uppercase or 0 otherwise
  */
-
 int _isupper(int c)
 {
 	if (isupper(c))

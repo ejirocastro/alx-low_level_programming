b247855a6a7eb6726586cae8c991fb2bdc0b65b8
ejirocastro <awenayeriejiro@gmail.com>
2023-07-25 16:55:38 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,9 +9,21 @@ void rev_string(char *s)
 {
 	int lent = 0;
 
+	int i = 0;
+
+	int j = lent - 1;
+
 	while (s[lent] != '\0')
 	{
 		lent++;
 	}
-	lent--;
+	while (i < j)
+	{
+		char trip = s[i];
+
+		s[i] = s[j];
+		s[j] = trip;
+		i++;
+		j--;
+	}
 }

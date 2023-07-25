216b584625744983cf4d0532b4f25562b23252ac
ejirocastro <awenayeriejiro@gmail.com>
2023-07-24 21:31:21 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,20 +8,17 @@
 
 void print_rev(char *s)
 {
-	int lent = strlen(s);
+	int len = strlen(s);
 
 	int i = 0;
 
-	int j = lent - 1;
-
-	while (i < j)
+	for (i = 0; i < len / 2; i++)
 	{
-		char temp = s[i];
+		char j = s[i];
 
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = j;
 	}
 	puts(s);
+
 }

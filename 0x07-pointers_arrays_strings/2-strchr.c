5a6bcf316c7fa9209ee542760e041f7058626c8c
ejirocastro <awenayeriejiro@gmail.com>
2023-07-31 18:38:06 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * _strchr - a function that locates a character
- * @s: pointer to aa character
- * @c: character pointed at
- * Return: s or 0.
+ * _strchr - function that fills memory with a constant byte
+ * @s: pointr
+ * @c: character to be looked for
+ * Return: 0 (success)
  */
-
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-	if (*s == c)
-	{
-	return (s);
-	}
-	s++;
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
 	return (0);
 }

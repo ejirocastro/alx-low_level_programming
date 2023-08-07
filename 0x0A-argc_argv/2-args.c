9b45a4d06fbdbdf4e64831888a0f70357672df65
ejirocastro <awenayeriejiro@gmail.com>
2023-08-06 20:48:08 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,4 +2,18 @@
 
 /**
  * main - Entry point
- * @agrc
+ * @argc: number of argument
+ * @argv: arguments
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}

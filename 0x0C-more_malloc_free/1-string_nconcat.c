9324bfd5274ed8c1e8228df9a787cfd9801c51e9
ejirocastro <awenayeriejiro@gmail.com>
2023-08-11 13:01:20 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	str = malloc(tot);
+	str = malloc(sizeof(char *) * (tot));
 
 	if (str == NULL)
 		return (NULL);

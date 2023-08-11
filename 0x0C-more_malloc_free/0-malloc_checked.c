7b6d1a3571712be63714c954657d421de551e0fb
ejirocastro <awenayeriejiro@gmail.com>
2023-08-10 19:13:45 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,14 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(b * sizeof(int));
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
+	else
+	{
 	return (ptr);
+	}
 }

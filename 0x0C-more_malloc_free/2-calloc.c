8f407ed663774c40e3916bdb62ad1d78f4db8468
ejirocastro <awenayeriejiro@gmail.com>
2023-08-11 20:54:02 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - function that allocates memory
+ * @nmemb: number of element
+ * @size: size in bytes
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(sizeof(int) * (nmemb * size));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	memset(ptr, 0, (nmemb * size));
+
+	return (ptr);
+}

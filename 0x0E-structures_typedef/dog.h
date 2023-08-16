a6945e4c35ee594bbe78221fab41152055d38eea
ejirocastro <awenayeriejiro@gmail.com>
2023-08-15 17:13:19 -0700

    0-main.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structue declaration
+ * @name: dogs name type char
+ * @age: dogs age type float
+ * @owner: dog owner type char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif

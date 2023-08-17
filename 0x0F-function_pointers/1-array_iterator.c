e22d25449ef956ca32446bd346f69b087d160b22
ejirocastro <awenayeriejiro@gmail.com>
2023-08-16 17:29:10 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,12 +5,14 @@
  * @array: hold elements
  * @size: size of array
  * @action: pointer function
+ * Return: void always
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x;
 
+	if (array != NULL && size > 0 && action != NULL)
 	for (x = 0; x < size; x++)
 	{
 		action(array[x]);

692b214b169f3873c082fb4df0a5250bfbf78d44
ejirocastro <awenayeriejiro@gmail.com>
2023-08-16 19:54:17 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argv: arguments passed
+ * @argc: argument count
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, nbytes;
+	unsigned char *code = (unsigned char *) main;
+
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (x = 0; x < nbytes; x++)
+	{
+		printf("%2x", code[x] & 0xFF);
+		if (x != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

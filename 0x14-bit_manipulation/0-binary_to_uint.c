ecb75c111cd7a1515c7589fc553c7bc0f9c0eb8a
ejirocastro <awenayericastrop@gmail.com>
2023-10-26 21:41:18 -0700

    bit operator task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to unsigned int
+ * @b: pointer to 0 and 1
+ * Return: he converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int temp = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (*b)
+	{
+		if (*b != '0' && *b != 'i')
+			return (0);
+		temp = (temp << 1) | (*b - '0');
+		i++;
+	}
+	return (temp);
+}

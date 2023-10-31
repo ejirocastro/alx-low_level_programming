62531a37851d7f0bf7417d83caf5191575c8588a
ejirocastro <awenayericastrop@gmail.com>
2023-10-30 20:17:23 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,8 +30,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		wr = write(file, &text_content[count], 1);
 		if (wr == -1)
+		{
 			return (-1);
+		}
 	}
+
 	close(file);
 	return (1);
 }
